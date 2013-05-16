f5aa6a247dc6dbd2d7f3b17d3e1abd239e080263
DsTroyed <teddy.freddy@live.com>
2013-05-16 12:46:28 +0300

    Update RandomSpawn.java

--- src/me/sirantony/minezchests/RandomSpawn.java
+++ src/me/sirantony/minezchests/RandomSpawn.java
@@ -1,10 +1,27 @@
 //to be dited here
 
 public class RandomSpawn{
+  private main pl;
+  public getItems(main instance) {
+		pl = instance;
+	}
   public void spawn(){
     getPlaces();
-  
+    for (Location loc : list){
+      loc.getBlock().setType(Material.chest);
+    }
   }
   public void Getplaces(){
-    
+    int respawn = pl.c.getConfig().getInt("spawnscheduler")*1200;
+    pl.getServer().getScheduler().syncRepeatinTask(main, {
+        run(){
+          World[] worlds = pl.getServer().getEnabledWorlds();
+          for (World world : worlds){
+            //...
+          }
+          //...
+          Location[] list;
+          return(list)
+        }
+      },respawn,respawn);
   }
